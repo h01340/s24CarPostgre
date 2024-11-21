6ba104a151106014f5a5df494cc9093431907086
Pellikka Minna <h01340@haaga-helia.fi>
2024-11-21 11:38:20 +0200

    changes to use postgresql db in render

--- src/main/java/k24/carWithPostegre/CarWithPostegreApplication.java
+++ src/main/java/k24/carWithPostegre/CarWithPostegreApplication.java
@@ -22,43 +22,4 @@ public class CarWithPostegreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarWithPostegreApplication.class, args);
 	}
-
-	 @Bean
-	public CommandLineRunner demoData(CarRepository carRepository,
-			OwnerRepository ownerRepository,
-			ApplicationUserRepository applicationUserRepository) {
-		return (args) -> {
-
-			log.info("luodaan pari sovelluksen käyttäjää: toinen admin/admin ja toinen user/user");
-			// public ApplicationUser(String firstName, String lastName, String role, String
-			// username, String passwordHash)
-			ApplicationUser user1 = new ApplicationUser("Otto", "Pellikka", "USER", "user",
-					"$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6");
-			ApplicationUser user2 = new ApplicationUser("Minna", "Pellikka", "ADMIN", "admin",
-					"$2a$08$bCCcGjB03eulCWt3CY0AZew2rVzXFyouUolL5dkL/pBgFkUH9O4J2");
-			applicationUserRepository.save(user1);
-			applicationUserRepository.save(user2);
-					
-			log.info("save owners");
-			ownerRepository.save(new Owner("Kia", "Watson", 1978));
-			ownerRepository.save(new Owner("Aku", "Ankka", 1940));
-			ownerRepository.save(new Owner("Iines", "Ankka", 1941));
-			log.info("print owners");
-			for (Owner owner : ownerRepository.findAll()) {
-				log.info(owner.toString());
-			}
-
-			log.info("save some cars");
-			carRepository.save(new Car("Ford", "Mustang", ownerRepository.findByLastName("Watson").get(0)));
-			carRepository.save(new Car("Nissan", "Leaf", ownerRepository.findByLastName("Watson").get(0)));
-			carRepository.save(new Car("Toyota", "Prius", ownerRepository.findByLastName("Ankka").get(0)));
-			carRepository.save(new Car("Toyota", "Prius"));
-
-			log.info("print car information");
-			for (Car car : carRepository.findAll()) {
-				log.info(car.toString());
-			}
-
-		};
-	}
 }
